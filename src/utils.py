fd8188947197457e5cb4ddd5587ea17f3bcc1e9a
venkatgmrit05 <venkat.gmrit05@gmail.com>
2023-03-12 00:46:47 -0500

    cleaned

--- src/utils.py
+++ src/utils.py
@@ -1,11 +1,12 @@
 import os
 import sys
 
-import numpy as np 
+import numpy as np
 import pandas as pd
 import dill
 
 from src.exception import CustomException
+
 
 def save_object(file_path, obj):
     try:
