6c37990c402c7602e1ac2dd38574fff871b32bd5
venkatgmrit05 <venkat.gmrit05@gmail.com>
2023-03-12 00:46:47 -0500

    cleaned

--- src/utils.py
+++ src/utils.py
@@ -1,13 +1,14 @@
 import os
 import sys
 
-import numpy as np 
+import numpy as np
 import pandas as pd
 import dill
 from sklearn.metrics import r2_score
 from sklearn.model_selection import GridSearchCV
 
 from src.exception import CustomException
+
 
 def save_object(file_path, obj):
     try:
